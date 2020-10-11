534241aaf1afac9738864fba453aed7a0fb2081f
Duncan Gray <duncan.gray@ga.gov.au>
2020-10-12 10:06:22 +1100

    setting up a crippled production

--- dags/nci_ard.py
+++ dags/nci_ard.py
@@ -22,12 +22,12 @@ params = {
     "/g/data/v10/projects/c3_ard/dea-ard-scene-select/scripts/prod/ard_env/index-datacube.env",
     "wagl_env": "/g/data/v10/projects/c3_ard/dea-ard-scene-select/scripts/prod/ard_env/prod-wagl.env",
     "config_arg": "",
-    "scene_limit": "",
+    "scene_limit": "--scene-limit 3",
     "products_arg": "",
     "pkgdir_arg": "/g/data/xu18/ga",
     "base_dir": "/g/data/v10/work/c3_ard/",
     "days_to_exclude_arg": """--days-to-exclude '["2020-08-09:2020-09-03"]'""",
-    "run_ard_arg": "--run-ard",
+    "run_ard_arg": "", #--run-ard
 }
 # params["days_to_exclude_arg"] = """--days-to-exclude '["2020-06-26:2020-06-26"]'"""
 ssh_conn_id = "lpgs_gadi"
